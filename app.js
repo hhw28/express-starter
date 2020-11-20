b8bb3b9a07bf004e6171185cdf3b13eabbadb8f4
wanghuan <196912334@qq.com>
2020-11-20 13:52:26 +0800

    hello world

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/xxx', (req, res) => res.send('hello world!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
